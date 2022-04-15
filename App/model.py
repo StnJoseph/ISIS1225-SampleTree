024149af57cb56678172f1866969c3e03d332b56
Joseph Linares <js.linares@uniandes.edu.co>
2022-04-15 15:34:06 -0500

    Lab 9 – Entrega final

--- App/model.py
+++ App/model.py
@@ -57,7 +57,7 @@ def newAnalyzer():
                 }
 
     analyzer['crimes'] = lt.newList('SINGLE_LINKED', compareIds)
-    analyzer['dateIndex'] = om.newMap(omaptype='BST',
+    analyzer['dateIndex'] = om.newMap(omaptype='RBT',
                                       comparefunction=compareDates)
     return analyzer
 
